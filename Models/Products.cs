0f6c65d17244bdef52e3731db8e462b42a19e28d
Britnay Gore <britnaygore268@gmail.com>
2024-08-24 10:49:42 -0400

    Created data models

---
+++ Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Bangazon_BE.Models;
+
+public class Products
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public decimal Price { get; set; }
+	public string Description { get; set; }
+	public int Quantity { get; set; }
+	public string ImageUrl { get; set; }
+	public int UserId { get; set; }
+	public Users User { get; set; }
+	public int CategoryId { get; set; }
+	public Categories Category { get; set; }
+}
+
